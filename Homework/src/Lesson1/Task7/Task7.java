db9dbac46bb17c13c8dd1e6afce676a501043906
Exicetin <77433018+Exicetin@users.noreply.github.com>
2022-03-18 15:19:53 +0300

    Add files via upload

--- Homework/src/Lesson1/Task7/Task7.java
+++ Homework/src/Lesson1/Task7/Task7.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 
 // Посчитать сумму столбцов и строк в массиве
 public class Task7 {
+/*
+    Подсчет суммы строк и столбцов
     private static void mass() {
         int[][] mas = {
                 {0, 0, 0, 0, 0, 0},
@@ -37,5 +39,31 @@ public class Task7 {
     }
 
 }
+*/
+//Доработка алгоритма записи рандомных чисел в массив
+    private static void mass() {
+        int[][] mas = new int[6][6];
 
+
+
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j < 5; j++){
+                mas[i][j] = (int)(Math.random()*10+1);
+                mas[i][mas[i].length - 1] += mas[i][j];
+                mas[mas.length-1][j] += mas[i][j];
+            }
+            System.out.println(Arrays.toString(mas[i]));
+        }
+        System.out.println("Сумма колонок");
+        System.out.println(Arrays.toString(mas[mas.length - 1]));
+
+    }
+
+    public static void main(String[] args) {
+        mass();
+
+    }
+
+}
 
